d22c3ea85436a431100ddbc5433a4d6c156cc41c
Yemer Ramos Ccorimanya <1005920191@unajma.edu.pe>
2023-09-29 08:43:46 -0500

    hicimos la creacion de la base de datos la conexion, hicimos un boton que crea la base de datos ademas creamos la tabal productos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import kivy
-
+import sqlite3
+import os
 from kivy.config import Config
 Config.set("graphics","width","340")
 Config.set("graphics", "height","640")
@@ -10,16 +11,48 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+def connect_to_database(path):
+    try:
+        con = sqlite3.connect(path)
+
+        cursor = con.cursor()
+        create_table_productos(cursor)
+        con.commit()
+        con.close()
+    except Exception as e:
+        print(e)
+
+def create_table_productos(cursor):
+    cursor.execute(
+        """
+        CREATE TABLE Productos(
+        ID          INT PRIMARY KEY      NOT NULL,
+        Nombre      TEXT                NOT NULL,
+        Marca       TEXT                NOT NULL,
+        Costos      FLOAT               NOT NULL,
+        Almacen     INT                 NOT NULL
+        )
+        """
+    )
+
 class MainWid(ScreenManager):
     def __init__(self, **kwargs):
         super(MainWid,self).__init__()
-        self.StartWid = StartWid()
+        self.APP_PATH = os.getcwd() 
+        self.DB_PATH = self.APP_PATH+"/my_database.db"
+        self.StartWid = StartWid(self)
+
         wid = Screen(name='start')
         wid.add_widget(self.StartWid)
         self.add_widget(wid)
 
 class StartWid(BoxLayout):
-    pass
+    def __init__(self, mainwid,**kwargs):
+        super(StartWid,self).__init__()
+        self.mainwid = mainwid 
+
+    def create_database(self):
+        connect_to_database(self.mainwid.DB_PATH)           
 
 class MainApp(App):
     title =' INventario SIMPLE'
