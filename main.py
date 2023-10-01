5773f5a4e7f9cf14837283ee692de26ce4411506
Yemer Ramos Ccorimanya <1005920191@unajma.edu.pe>
2023-10-01 00:10:50 -0500

    hicimos lo que es el salto de la pantalla principal a otra secundaria

--- main.py
+++ main.py
@@ -29,22 +29,42 @@ def create_table_productos(cursor):
         ID          INT PRIMARY KEY      NOT NULL,
         Nombre      TEXT                NOT NULL,
         Marca       TEXT                NOT NULL,
-        Costos      FLOAT               NOT NULL,
+        Costo      FLOAT               NOT NULL,
         Almacen     INT                 NOT NULL
         )
         """
     )
 
+# este es el wid primcipal aca se deb agragar un boton o un afuncion de algo para que aparesca en la pantalla principal
 class MainWid(ScreenManager):
     def __init__(self, **kwargs):
         super(MainWid,self).__init__()
         self.APP_PATH = os.getcwd() 
         self.DB_PATH = self.APP_PATH+"/my_database.db"
         self.StartWid = StartWid(self)
+        self.DataBaseWid =  DataBaseWid(self)
 
         wid = Screen(name='start')
         wid.add_widget(self.StartWid)
         self.add_widget(wid)
+
+        #aca tambien lo agregamos para poderlo invocar mas tarde
+        wid = Screen(name='database')
+        wid.add_widget(self.DataBaseWid)
+        self.add_widget(wid)
+
+        self.goto_start()
+    
+    
+    # estas funciones haran que se entre a una interfas diferente del interfas principal
+    def goto_start(self):
+
+        self.current = 'start'
+
+    def goto_database(self):
+
+        self.current = 'database'
+
 
 class StartWid(BoxLayout):
     def __init__(self, mainwid,**kwargs):
@@ -52,7 +72,19 @@ class StartWid(BoxLayout):
         self.mainwid = mainwid 
 
     def create_database(self):
-        connect_to_database(self.mainwid.DB_PATH)           
+        connect_to_database(self.mainwid.DB_PATH)
+
+        # en esta linea invocamaos el cambio de pantallas
+        self.mainwid.goto_database()  
+
+#es el boton que contine la base de datos y que sera para agregar mas datos
+# hera de Boxlayaut por eso tien como parametro este nombre       
+class DataBaseWid(BoxLayout):
+    
+    def __init__(self, mainwid, **kwargs):
+        super(DataBaseWid, self).__init__()
+        self.mainwid = mainwid
+
 
 class MainApp(App):
     title =' INventario SIMPLE'
